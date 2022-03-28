b270255d5d74bb8de007d120759b241eb5cafcaa
904365002 <904365002@L815WD220013.edu.cbe.ab.ca>
2022-03-28 09:59:42 -0600

    Last Date of this Revision fixed.

--- Chapter5/src/AccountSetup.java
+++ Chapter5/src/AccountSetup.java
@@ -3,7 +3,7 @@ import java.util.concurrent.TimeUnit;
 
 /*
 
-Program: AccountSetup.java          Last Date of this Revision: 03, 18, 2022
+Program: AccountSetup.java          Last Date of this Revision: 03, 28, 2022
 
 Purpose: Create an AccountSetup application that prompts the user for a user name and a password.
 		 The application should prompt the user until a password with at least eight characters is entered.
