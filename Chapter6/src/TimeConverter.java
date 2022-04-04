d03462f9f91293957860f88be798823f3e09dd09
904365002 <904365002@L815WD220013.edu.cbe.ab.ca>
2022-04-04 09:36:56 -0600

    Added Time converter

---
+++ Chapter6/src/TimeConverter.java
@@ -0,0 +1,26 @@
+
+/*
+
+Program: AccountSetup.java          Last Date of this Revision: 03, 28, 2022
+
+Purpose: Create an AccountSetup application that prompts the user for a user name and a password.
+		 The application should prompt the user until a password with at least eight characters is entered.
+		 The user name and password should be converted to all lower case letters and then an appropriate message displayed.
+
+Author: Rowan Sallay, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
+public class TimeConverter {
+
+}
+
+
+/*
+
+
+
+*/
