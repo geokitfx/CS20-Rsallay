8077203149f1f0edd0ca3fa08905acc7eb66115d
904365002 <904365002@L815WD220013.edu.cbe.ab.ca>
2022-04-04 10:25:08 -0600

    Saving

--- Chapter6/src/TimeConverter.java
+++ Chapter6/src/TimeConverter.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 
 /*
 
@@ -16,7 +17,24 @@ Course: Computer Programming 20
 
 public class TimeConverter {
 
+	public static void main(String[] args) {
+		 
+		 String name = JOptionPane.showInputDialog(null,
+		 "What is your name?");
+		 
+		 int choice = JOptionPane.showConfirmDialog(null, "Do you like cake, " + name + "?");
+		 
+		 if (choice == JOptionPane.YES_OPTION) 
+		 {
+		 JOptionPane.showMessageDialog(null, "Of course! Who doesn't?");
+		 } 
+		 else 
+		 { 
+		 JOptionPane.showMessageDialog(null, "We'll have to agree to disagree.");
+		 }
+	}
 }
+
 
 
 /*
