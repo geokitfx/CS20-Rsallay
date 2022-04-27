2a9af349aa9e1d8f6b9bb8881c8240d71ad3df53
904365002 <904365002@L815WD220013.edu.cbe.ab.ca>
2022-04-27 09:46:03 -0600

    added chapter 7

---
+++ Chapter7/src/TimeConverter/TimeConverter.java
@@ -0,0 +1,80 @@
+package TimeConverter;
+import java.awt.*;
+import javax.swing.*;
+
+
+/*
+
+Program: TimeConverter.java          Last Date of this Revision: 04, 11, 2022
+
+Purpose: Create a TimeCoverter application that allows the use to choose among converting hours to minutes,
+		 days to hours, minutes to hours, or hours to days. Use methods as appropriate.
+
+Author: Rowan Sallay, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
+public class TimeConverter {
+
+	public static void main(String[] args) {
+		 
+		JFrame frame = new JFrame();
+		frame.setForeground(Color.WHITE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocation(new Point(10, 50));
+		frame.setSize(new Dimension(1600, 900)); // W,H
+		frame.setTitle("Time Converter");
+		frame.setVisible(true);
+//---------------------------------------------------------------// 
+		JButton htm = new JButton();
+		htm.setText("Hours to Minutes");
+		htm.setBackground(Color.WHITE);
+		htm.setBounds(0,0,150, 40);   // X,Y L,W
+		frame.setLayout(null); 
+		frame.setVisible(true);
+		htm.addActionListener(new HTM());
+//---------------------------------------------------------------// 
+		JButton dth = new JButton();
+		dth.setText("Days to Hours");
+		dth.setBackground(Color.WHITE);
+		dth.setBounds(0,40,150, 40);   
+		frame.setLayout(null); 
+		frame.setVisible(true);
+		dth.addActionListener(new DTH());
+//---------------------------------------------------------------// 
+		JButton mth = new JButton();
+		mth.setText("Minutes to Hours");
+		mth.setBackground(Color.WHITE);
+		mth.setBounds(150,0,150, 40);   
+		frame.setLayout(null); 
+		frame.setVisible(true);
+		mth.addActionListener(new MTH());
+//---------------------------------------------------------------//
+		JButton htd = new JButton();
+		htd.setText("Hours to Days");
+		htd.setBackground(Color.WHITE);
+		htd.setBounds(150,40,150, 40);   
+		frame.setLayout(null); 
+		frame.setVisible(true);
+		htd.addActionListener(new HTD());
+//---------------------------------------------------------------//
+		frame.setLayout(new FlowLayout());
+		frame.add(htm);
+		frame.add(dth);
+		frame.add(mth);
+		frame.add(htd);
+//---------------------------------------------------------------//
+
+	}
+}
+
+
+
+/*
+
+
+
+*/
---
+++ Chapter7/src/exercise3/PrimeNumber.java
@@ -0,0 +1,66 @@
+package exercise3;
+
+/*
+
+Program: PrimeNumber.java          Last Date of this Revision: 04, 14, 2022
+
+Purpose: Modify the PrimeNumber application created in Chapter 5 Exercise 1 to include a method named isPrime().
+		 The isPrime() method should require one parameter and return a Boolean value.
+
+Author: Rowan Sallay, 
+School: CHHS
+Course: Computer Programming 20
+
+*/
+
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+/* It says to modify it to included a method named isPrime(), Never said i had to have anything else did it? 
+ Please note, I spent like half in hour trying to figure out how to split the code in half to use it in both main and isPrime.
+ But was unable to get that to work this is the next best solution I could come up with, Sorry!
+*/
+
+	public static void main(String[] args) {
+		isPrime();
+	}
+	
+	static void isPrime() 
+	{
+		System.out.println("Enter Number: ");
+		Scanner mvalue = new Scanner(System.in);
+		int num = mvalue.nextInt();
+		
+		mvalue.close();
+		
+	    boolean flag = false;
+	    for (int i = 2; i <= num / 2; ++i) {
+	      if (num % i == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+
+	    if (!flag)
+	      System.out.println(num + " is a prime number.");
+	    else
+	      System.out.println(num + " is not a prime number.");
+	    
+	}
+	
+}
+
+/* Screen Dump
+
+Enter Number: 
+4
+4 is not a prime number.
+
+Enter Number: 
+2
+2 is a prime number.
+
+
+*/
