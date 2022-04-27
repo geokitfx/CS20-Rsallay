6da7fb0a412a408159a5522103500efbd5727f62
904365002 <904365002@L815WD220013.edu.cbe.ab.ca>
2022-04-27 10:11:30 -0600

    save

---
+++ Chapter7/src/main/ReviewCircle.java
@@ -0,0 +1,15 @@
+package main;
+
+public class ReviewCircle {
+
+	public static void main(String[] args) {
+		Circle spot = new Circle();
+		
+		spot.setRadius(5);
+		
+		System.out.println("Circle radius " + spot.getRadius());
+		System.out.println("Circle area " + spot.area);
+
+	}
+
+}
