a27af08acb56e654124cf7098d20f6975e28ae8c
904365002 <904365002@L815WD220013.edu.cbe.ab.ca>
2022-05-30 09:25:30 -0600

    save

--- Chapter9/src/studentRoster/StudentRoster.java
+++ Chapter9/src/studentRoster/StudentRoster.java
@@ -6,26 +6,38 @@ public class StudentRoster {
 
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
+		Scanner num = new Scanner(System.in);
+		Scanner name = new Scanner(System.in);
 		int arrayindexS;
 		String[] ROSTER;
 		
 		System.out.println("How many Students?");
-		arrayindexS = input.nextInt();
+		arrayindexS = num.nextInt();
 		ROSTER = new String[arrayindexS];
 		
+		for (int n = 0; n < arrayindexS; n++)
+		{
+			System.out.println("Student Names?");
+			String studentName = name.nextLine();
+			ROSTER[n] = studentName;
+		}
+		
+			System.out.println("Student Roster");
+		for (int i = 0; i < arrayindexS; i++)
+		{
+			System.out.println("Student "+ i +": "+ROSTER[i]);
+		}
+		
+		num.close();
+		name.close();
+	}
+
+}
+
+/*
 		ROSTER[0] = "Wilbur";
 		ROSTER[1] = "Dream";
 		ROSTER[2] = "TechnoBlade";
 		ROSTER[3] = "Tommyinit";
 		ROSTER[4] = "Skeppy";
-		
-		for (int i = 0; i < arrayindexS; i++)
-		{
-			System.out.println(ROSTER[i]);
-		}
-		
-
-	}
-
-}
+*/
