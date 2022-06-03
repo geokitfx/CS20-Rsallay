5a1af6c9303007c70a60ae6e5b0fdeb38bb338a1
904365002 <904365002@L815WD220013.edu.cbe.ab.ca>
2022-06-03 09:39:20 -0600

    Chapter 9 Dice Rolls

--- Chapter9/src/dice/DiceRolls.java
+++ Chapter9/src/dice/DiceRolls.java
@@ -1,10 +1,45 @@
 package dice;
 
+/*
+UNABLE TO FINISH
+*/
+
+import java.util.Scanner;
+
 public class DiceRolls {
 
 	public static void main(String[] args) {
 		
+		int[] outcomes = new int[13];
+		Scanner input = new Scanner(System.in);
+		int numRolls;
+		int outcome;
+		int numDice;
+		
+		System.out.print("How many Dice? ");
+		numDice = input.nextInt();
+		
+		System.out.print("How many rolls? ");
+		numRolls = input.nextInt();
+		
+		for (int roll = 0; roll < numRolls; roll++)
+		{
+			outcome = (int)(6 * Math.random() + 1) + (int)(6 * Math.random() + 1);
+			outcomes[outcome] += 1;
+		}
+		
+		for (int i = 2; i <= 12; i++)
+		{
+			System.out.println(i + ": " + outcomes[i]);
+		}
+		
+		input.close();
 
 	}
 
 }
+
+
+/*
+UNABLE TO FINISH
+*/
